6310161613888185c98c67f1f7dd8c0525a4a40d
somnath46 <4684.somnath@gmail.com>
2020-07-07 08:15:43 +0530

    controller with stub methods closes #3

---
+++ src/main/java/com/tnt/brewery/web/controller/BeerController.java
@@ -0,0 +1,38 @@
+package com.tnt.brewery.web.controller;
+
+import java.util.UUID;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.tnt.brewery.model.BeerDto;
+
+@RestController
+@RequestMapping("/api/v1/beer")
+public class BeerController {
+
+	@GetMapping("/{beerId}")
+	public ResponseEntity<BeerDto> getBeer(@PathVariable final UUID beerId) {
+		// TODO: Implementation
+		return new ResponseEntity<BeerDto>(BeerDto.builder().build(), HttpStatus.OK);
+	}
+
+	@PostMapping
+	public ResponseEntity createBear(@RequestBody final BeerDto beerDto) {
+		// TODO: Implementation
+		return new ResponseEntity(HttpStatus.CREATED);
+	}
+
+	@PutMapping("/{beerId}")
+	public ResponseEntity updateBear(@PathVariable final UUID beerId, @RequestBody final BeerDto beerDto) {
+		// TODO: Implementation
+		return new ResponseEntity(HttpStatus.NO_CONTENT);
+	}
+}
